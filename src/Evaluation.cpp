d0245426c3c0319e9ef5833f3b045eef34fe395c
Tarique Mackay <tariquemackay@gmail.com>
2026-01-03 00:54:01 +0200

    implemented piece square tables for pawn, knights and bishops to improve evaluation

--- src/Evaluation.cpp
+++ src/Evaluation.cpp
@@ -4,18 +4,70 @@
 
 int Evaluation::evaluate(Board &board) {
     int score = 0;
+    int pieceCount = 0;
 
-    // sum up material score for white
-    for (int piece = WP; piece <= WK; piece++){
-        int pieceCount = popCount(board.bitboards[piece]);
-        score += pieceCount * pieceValues[piece];
+    // --- white evaluation --
+
+    // pawn points
+    U64 bitboard = board.bitboards[WP];
+    while (bitboard) {
+        int piecePosition = popLSB(bitboard);
+        score += pieceValues[WP] + pawnTable[piecePosition]; // material score + PST score
     }
 
-    // subtract sum up material score for black
-    for (int piece = BP; piece <= BK; piece++){
-        int pieceCount = popCount(board.bitboards[piece]);
-        score -= pieceCount * pieceValues[piece-6];
+    // knight points
+    bitboard = board.bitboards[WN];
+    while (bitboard) {
+        int piecePosition = popLSB(bitboard);
+        score += pieceValues[WN] + knightTable[piecePosition]; // material score + PST score
     }
+
+    // king points
+    bitboard = board.bitboards[WK];
+    while (bitboard) {
+        int piecePosition = popLSB(bitboard);
+        score += pieceValues[WK] + kingTable[piecePosition]; // material score + PST score
+    }
+
+    // sum up material score for white bishops
+    pieceCount = popCount(board.bitboards[WB]);
+    score += pieceCount * pieceValues[WB];
+
+    // sum up material score for white queens
+    pieceCount = popCount(board.bitboards[WQ]);
+    score += pieceCount * pieceValues[WQ];
+    
+
+    // --- black evaluation --
+
+    // pawn points
+    bitboard = board.bitboards[BP];
+    while (bitboard) {
+        int piecePosition = popLSB(bitboard);
+        score -= pieceValues[BP-6] + pawnTable[piecePosition^56]; // material score + PST score
+    }
+
+    // knight points
+    bitboard = board.bitboards[BN];
+    while (bitboard) {
+        int piecePosition = popLSB(bitboard);
+        score -= pieceValues[BN-6] + knightTable[piecePosition^56]; // material score + PST score
+    }
+
+    // king points
+    bitboard = board.bitboards[BK];
+    while (bitboard) {
+        int piecePosition = popLSB(bitboard);
+        score -= pieceValues[BK-6] + kingTable[piecePosition^56]; // material score + PST score
+    }
+
+    // sum up material score for black bishops
+    pieceCount = popCount(board.bitboards[BB]);
+    score -= pieceCount * pieceValues[BB-6];
+
+    // sum up material score for black queens
+    pieceCount = popCount(board.bitboards[BQ]);
+    score -= pieceCount * pieceValues[BQ-6];
 
     // return positive score for white and negative for black
     return (board.activeColour == WHITE) ? score : -score;
